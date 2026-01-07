b11842072cb62a0fd27dbb5efddbb2bc2fc60e43
dahsar-5 <dahsar-5@student.ltu.se>
2026-01-07 16:10:20 +0100

    Uppdaterat kommentarer
    Added getter and setter for 'usable' property in Key class.

--- src/DragonTreasure/Key.java
+++ src/DragonTreasure/Key.java
@@ -9,16 +9,18 @@ public class Key extends Item {  //Nyckel är en subklass til item
         this.usable = usable;
     }
 
+   // Getter för att kolla om nyckeln är användbar
     public boolean isUsable() { 
         return usable;
     }
 
+    //Setter för att ändra om nyckeln är användbar eller inte
     public void setUsable(boolean usable) { 
         this.usable = usable;
     }
 
    @Override
-public void use(Player player) {
+public void use(Player player) { //Metod: när spelaren har en nyckel som är användbar och använder den
     if (usable) {
         System.out.println("Du kan nu använda " + getName() + "n för att låsa upp en dörr.");
         player.giveKey();   
