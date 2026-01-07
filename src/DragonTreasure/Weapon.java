cb19c88c1dac4320a86f58c3e41f3b1428edbd18
dahsar-5 <dahsar-5@student.ltu.se>
2026-01-07 16:12:47 +0100

    Uppdaterat kommentarer
    Added comment to clarify constructor and use method.

--- src/DragonTreasure/Weapon.java
+++ src/DragonTreasure/Weapon.java
@@ -5,12 +5,12 @@ public class Weapon extends Item {
     private int damageMultiplier; //Vapen kan öka spelarens skada med damageMultiplier
 
     public Weapon(String name, String description, int damageMultiplier) {
-        super(name, description); //
+        super(name, description); 
         this.damageMultiplier = damageMultiplier;
     }
 
     @Override
-    public void use(Player player) { 
+    public void use(Player player) { //Metod för att öka spelarens skada med vapnet
         int newDamage = player.getDamage() * damageMultiplier;
         player.setDamage(newDamage);
         System.out.println("Du lyfter upp " + getName() + "! Din skada är nu " + newDamage + ".");
